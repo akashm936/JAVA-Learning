037c132a01fe91ea8279f6f71c6a4f8b5fa66113
Akash Morwal <akashbhau101@gmail.com>
2024-03-12 15:13:08 +0530

    new program

--- IndecExample.java
+++ IndecExample.java
@@ -1,5 +1,14 @@
 class IndecExample
 {
 	//Incriment and Decriment operators exmaple and there program
-	
+    public static void main(String[] args) {
+    		
+
+    		int a = 56;
+    		int b = ++a; //57
+    		int c = ++b; // 58
+
+              //                
+    		System.out.println(a++ + a + --b + c++ + a);
+    	}	
 }
