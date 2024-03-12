de659d74cd6aa73600e443ed906673515c341bcc
Akash Morwal <akashbhau101@gmail.com>
2024-03-12 15:22:55 +0530

    new program execute

--- IndecExample.java
+++ IndecExample.java
@@ -3,12 +3,23 @@ class IndecExample
 	//Incriment and Decriment operators exmaple and there program
     public static void main(String[] args) {
     		
+            //increment Example
+    		// int a = 56;
+    		// int b = ++a; //57
+    		// int c = ++b; // 58
 
-    		int a = 56;
-    		int b = ++a; //57
-    		int c = ++b; // 58
+            //   //                
+    		// System.out.println(a++ + a + ++b + c++ + a);
 
-              //                
-    		System.out.println(a++ + a + --b + c++ + a);
-    	}	
+
+
+    		//decriment Example
+
+
+    		char d = 'b';
+    		int e = 67;
+    		int f = 'c';
+
+    		System.out.println(d + --e + --f + d);
+     	}	
 }
