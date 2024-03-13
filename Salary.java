b58480cdf990c80752bce77c308a303b99085821
Akash Morwal <akashbhau101@gmail.com>
2024-03-13 19:29:10 +0530

    done

--- Salary.java
+++ Salary.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
 
 class Salary
-{
+{   
+    //this program is succesfully run
    public static void main(String[] args) {
    		
    		Scanner sc = new Scanner(System.in);
+         
+
+         System.out.println("Enter the Salary of Employee : ");
+         double b = sc.nextDouble();
 
    		System.out.println("Enter the Score of Employee : ");
    	    double s = sc.nextDouble();
 
         if(s > 90)
         {
-        	 s = s/3;
-        	System.out.println( s + " 3 % is increases ");
+        	 b = b*3/100;
+        	System.out.println( b + " 3 % is increases ");
         }
         else
         {
-           s =   s/1;
-           System.out.println(s + " 1 %  is increases");
+           b = b*1/100;
+           System.out.println(b + " 1 %  is increases");
         }
    	}	
 }
