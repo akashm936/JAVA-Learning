9884e28b0bd369552affeed53531131164f2798f
Akash Morwal <akashbhau101@gmail.com>
2024-03-16 22:52:41 +0530

    16-03-2024 assignment done

---
+++ Month.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Month
+{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Month Number : ");
+		int monthNum = sc.nextInt();
+
+		String month = null;
+
+		switch(monthNum)
+		{
+		case 1 : month = " JAN "; break;
+		case 2 : month = " FEB "; break;
+		case 3 : month = " MAR "; break;
+		case 4 : month = " APR "; break;
+		case 5 : month = " MAY "; break;
+		case 6 : month = " JUN "; break;
+		case 7 : month = " JALY "; break;
+		case 8 : month = " AUG "; break;
+		case 9 : month = " SEP "; break;
+		case 10 : month = " OCT "; break;
+		case 11 : month = " NOV "; break;
+		case 12 : month = " DEC "; break;
+		default : System.out.println("Invalid Input");
+		}
+		System.out.println((month != null) ?(monthNum + " - " + month ): "");
+	}
+}
