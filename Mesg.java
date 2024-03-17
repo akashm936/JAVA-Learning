694c674475f6cc313e6e0f26be7b41f74672e121
Akash Morwal <akashbhau101@gmail.com>
2024-03-17 16:02:42 +0530

    new program done (17-03-2024)

---
+++ Mesg.java
@@ -0,0 +1,12 @@
+class Mesg
+{
+	public static void main(String[] args) {
+		
+		String msg= "Hello Akash, how Are You?";
+
+		for(int i=0; i<=1000;i++)
+		{
+			System.out.println(msg);
+		}
+	}
+}
