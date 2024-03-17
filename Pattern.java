1810e3881e49e4eda427cc4fd41719081ae8102d
Akash Morwal <akashbhau101@gmail.com>
2024-03-17 18:05:52 +0530

    update (17-03-2024)

--- Pattern.java
+++ Pattern.java
@@ -8,7 +8,7 @@ class Pattern
 		{
             for(j=0;j<=i;j++)
             {
-            	System.out.print("*");
+            	System.out.print(" * ");
             }
 
             System.out.println();
