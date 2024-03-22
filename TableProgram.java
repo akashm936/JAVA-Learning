fb43224314fda235f8adb8aacf6476f9b9c1e96b
Akash Morwal <akashbhau101@gmail.com>
2024-03-22 14:44:06 +0530

    new modification (22-03-2024)

--- TableProgram.java
+++ TableProgram.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class TableProgram
 {
+	//this program is made for create a 19 pahada(table)
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
