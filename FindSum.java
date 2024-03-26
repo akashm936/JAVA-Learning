e98f18f634e1c1f84be3e9783e1d262d08f57fdd
Akash Morwal <akashbhau101@gmail.com>
2024-03-26 21:17:32 +0530

    New Program Added (26-03-2024)

--- FindSum.java
+++ FindSum.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class FindSum
 {
-	// 
+	// Find sum program 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
---
+++ SwapNumber.java
@@ -0,0 +1,24 @@
+
+class SwapNumber
+{
+	public static void main(String[] args) {
+		
+		float num1 = 64.5f;
+		float num2 = 45.6f;
+
+		System.out.println("-- Before Swapping --");
+		System.out.println("First Number = "+ num1);
+		System.out.println("Second Number = "+ num2);
+
+
+		float temp = num1;// we create a temp value for storing data
+
+		num1 = num2;
+
+		num2 = temp;
+
+		System.out.println(" --After Swapping-- ");
+		System.out.println(" First Number = "+num1);
+		System.out.println(" Second Number = "+num2);
+	}
+}
