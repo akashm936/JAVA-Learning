a3ece7ec0c48a244d10969f742fe3da5653ae5e5
Akash Morwal <akashbhau101@gmail.com>
2024-03-27 00:18:17 +0530

    New Program of Ascii Value no printing Updated (27-03-2024)

---
+++ Example5.java
@@ -0,0 +1,15 @@
+class Example5
+{
+	public static void main(String[] args) {
+		
+		//int asciiValue = 65;
+
+		for(char ch='A';ch<='Z';ch++)
+		{
+			System.out.println((ch+0)+ "->" +ch + " " );
+
+		//	System.out.println(asciiValue++ + "->" + ch+" ");
+		//	System.out.println((int)ch+ "->"+ch+" ");
+		}
+	}
+}
