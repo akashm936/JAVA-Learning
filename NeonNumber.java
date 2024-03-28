0b0b11b78950f07f178af2b4de2b237950d9fb27
Akash Morwal <akashbhau101@gmail.com>
2024-03-28 10:29:58 +0530

    new Program of Neon Number is updated (28-03-2024)

---
+++ NeonNumber.java
@@ -0,0 +1,21 @@
+class NeonNumber
+{
+	public static void main(String[] args) {
+		int num = 1;
+		int sqr = num*num;
+		int sum = 0;
+		while(sqr>0)
+		{
+			int rem = sqr%10;
+			sum +=rem;
+			sqr/=10;
+		}
+		if(sum==num)
+		{
+			System.out.println(num + " is Neon Number");
+		}
+		else{
+			System.out.println(num + " is Not a Neon Number");
+		}
+	}
+}
