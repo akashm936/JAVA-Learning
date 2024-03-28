ca5acd9ca12eefabb74019e0f026221afbcc6f26
Akash Morwal <akashbhau101@gmail.com>
2024-03-28 13:59:46 +0530

    new Program of pacinating Number (28-03-2024)

---
+++ PacinatingNumber.java
@@ -0,0 +1,12 @@
+class PacinatingNumber
+{
+
+	//this program is Pacinating Number program
+	public static void main(String[] args) {
+		
+		int num1 = 327;
+		int num2 = num1*2;
+		int num3 = num1*3;
+		System.out.println(""+num1+num2+num3);
+	}
+}
