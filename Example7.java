7d284ca39b5e91470a8f1df80e587068588e64ea
Akash Morwal <akashbhau101@gmail.com>
2024-03-28 13:36:26 +0530

    New Program of  Buzz Number is Updated ( 28-03-2024)

--- Example7.java
+++ Example7.java
@@ -6,25 +6,36 @@ class Example7
 		
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter a Number : ");
-		int num = sc.nextInt();
-		int length = 0;
-
-		for(int i=1;i<=num;i++)
-		{
-			if(num/7){
-				System.out.println(num + "this is bus number");
-			}
-			else{
-				int rem = num%10;
-				length++;
-				if(rem<=7)
-				{
-					System.out.println("this numebr is bus number");
-				}
-                
-				num/=10;
-			}
+		System.out.print("Enter a Number : ");
+		int num =sc.nextInt();
+		if((num%7==0) || (num%10==7)){
+			System.out.println(num + " it is a Buzz number ");
+		}
+		else{
+			System.out.println(num + " it is not a Buzz number ");
 		}
 	}
 }
+
+
+
+// System.out.println("Enter a Number : ");
+// 		int num = sc.nextInt();
+// 		int length = 0;
+
+// 		for(int i=1;i<=num;i++)
+// 		{
+// 			if(num%7==0){
+// 				System.out.println(num + "this is bus number");
+// 			}
+// 			else{
+// 				int rem = num%10;
+// 				length++;
+// 				if(rem<=7)
+// 				{
+// 					System.out.println("this numebr is bus number");
+// 				}
+                
+// 				num/=10;
+// 			}
+// 		// }
