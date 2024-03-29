0c1bd094d21d757294598b380af99dd800cae723
Akash Morwal <akashbhau101@gmail.com>
2024-03-29 08:01:19 +0530

    new Program updated (29-03-2024)

--- SunnyNumber.java
+++ SunnyNumber.java
@@ -8,11 +8,18 @@ class SunnyNumber
 		Scanner sc = new Scanner(System.in);
         
         System.out.println("Enter a Number : ");
-		int num = sc.nextInt();
-		boolean flag = false;
-		for(int i=1;i<=num;i++)
-		{
-			if(num*num)
-		}
+        int num = sc.nextInt();
+
+        double sqrrt , d;
+
+        sqrrt=Math.sqrt(num+1);
+        //d=sqrrt.Math.floor(sqrrt);
+        if(d==0.0)
+        {
+        	System.out.println(num + " is a Sunny Number");
+        }
+        else{
+        	System.out.println(num + " is Not a Sunny Number ");
+        }
 	}
 }
