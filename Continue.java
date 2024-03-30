7c625e330309296737d090cd314f8c23edf68772
Akash Morwal <akashbhau101@gmail.com>
2024-03-30 20:25:50 +0530

    new Program updated (30-03-2024)

--- Continue.java
+++ Continue.java
@@ -1,10 +1,12 @@
 class Continue
 {
+	//this program show unreacheble statement error
 	public static void main(String[] args) {
 		
 		for(int i = 1;i<=100;i++)
 		{
-			continue;
+			continue; //becouse the continue keyword skip the print statement
+			
 			System.out.print(i + " ");
 		}
 	}
