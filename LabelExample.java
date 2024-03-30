fb7034304f5f3f88a6ff09f91919bc1a2c8d0d89
Akash Morwal <akashbhau101@gmail.com>
2024-03-30 19:56:32 +0530

    New Program Updated (30-03-2024)

---
+++ LabelExample.java
@@ -0,0 +1,21 @@
+class LabelExample
+{
+	public static void main(String[] args) {
+		
+		//ramesh:
+		for (int i=1;i<=2;i++)
+		{
+			for(int j=1;j<=3;j++)
+			{
+				if(j==3);
+				{
+					System.out.println("Hello Akash, How are You? ");
+					System.out.println();
+					System.out.println("i - "+ i +" | " + "j - "+ j);
+				   	break ;//ramesh;
+				}
+				
+			}
+		}
+	}
+}
