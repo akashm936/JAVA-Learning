a8981efc8242ab96be44e706c0a991e8091a4298
Akash Morwal <akashbhau101@gmail.com>
2024-04-02 15:11:05 +0530

    new program are updated of krishnamurthy Number (02-04-2024)

---
+++ KrishnamurthyNumber.java
@@ -0,0 +1,46 @@
+//KrishnamurthyNumber.java
+import java.util.*;
+
+class KrishnamurthyNumber
+{
+	public static void main(String[] args) {
+	
+	  Scanner sc = new Scanner(System.in);
+	  int n;
+	  System.out.print("Enter a Number : ");
+
+	  n = sc.nextInt();
+
+	  if(checkNumeber(n))
+	      System.out.println(n+ " is Krishnamurthy Number");	
+	  else
+	  	System.out.println(n + " is Not Krishnamurthy Number");
+	}
+	public static int fact(int num){
+
+     int f =1;
+     while(num !=0){
+     	f = f * num;
+     	num--;
+     }
+     return f;
+	}
+
+	public static boolean checkNumeber(int num)
+	{
+		int sum = 0;
+		int tempNumber = num;
+
+		while(tempNumber !=0){
+			sum = sum + fact(tempNumber % 10);
+			tempNumber = tempNumber /10;
+		}
+
+		if(sum == num)
+			return true;
+		else
+			return false;
+	}
+
+
+}
