8015e6da80a868acdf2789852fc7cda4e70c6877
Akash Morwal <akashbhau101@gmail.com>
2024-04-11 15:38:00 +0530

    new program modify (11-04-2024)

--- StarPatternPractice.java
+++ StarPatternPractice.java
@@ -2,11 +2,11 @@ class StarPatternPractice
 {
 	public static void main(String[] args) {
 		
-		for(int i=1;i<6;i++)
+		for(int i=1;i<=6;i++)
 		{
 			for(int j=1;j<=i-1;j++)
 			{
-				System.out.print(" ");
+				System.out.print("  ");
 			}
 			for(int k=5;k>=i-1;k--)
 			{
