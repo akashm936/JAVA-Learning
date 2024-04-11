a465ecf5ed1d13a6ed5ab60b72104014410489ea
Akash Morwal <akashbhau101@gmail.com>
2024-04-11 15:42:07 +0530

    new program updated (11-04-2024)

--- StarPattern4.java
+++ StarPattern4.java
@@ -4,14 +4,16 @@ class StarPattern4
 		
 		for(int i=1;i<=5;i++)
 		{
-			for(int j=5;j>=i;j--)
-			{
-				System.out.print("* ");
-			}
 			for(int j=1;j<=i-1;j++)
 			{
 				System.out.print("  ");
 			}
+			
+			for(int j=5;j>=i;j--)
+			{
+				System.out.print("* ");
+			}
+			
 			
 			
 			System.out.println();
