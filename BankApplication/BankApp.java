a8b92efd1bcb254a83d823e48c69cc14516b2144
Akash Morwal <akashbhau101@gmail.com>
2024-04-29 14:29:45 +0530

    new code Updated (29-04-2024)

--- BankApplication/BankApp.java
+++ BankApplication/BankApp.java
@@ -12,7 +12,15 @@ class BankApp
 
     public static void main(String args [])
     {
+    	//this is main method
 
+    	Scanner sc = new Scanner(System.in);
+        
+    	System.out.println("This is Simple bank Application");
+
+    	System.out.println("Select Option \n1. Create New Account. \n2. Existing Account.");
+        
+        
     } 
 
     public static void createAccount(){
