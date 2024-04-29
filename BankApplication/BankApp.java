580ec45f8db44b89453922d4aaedb23ccb263af2
Akash Morwal <akashbhau101@gmail.com>
2024-04-29 14:26:37 +0530

    new Method updated (29-04-2024)

---
+++ BankApplication/BankApp.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class BankApp
+{
+	private String accountHolderName;
+	private String address;
+	private long aadharNumber;
+	private String panNumber;
+	private double balance;
+	private long phoneNumber;  
+    
+
+    public static void main(String args [])
+    {
+
+    } 
+
+    public static void createAccount(){
+
+    	System.out.println("");
+    }
+
+    public void getaccountHolderName
+}
+
+
+
