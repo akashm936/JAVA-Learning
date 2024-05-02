29f5e6ccc8f74019a0782a8846e888f62b02cf17
Akash Morwal <114584845+akashm936@users.noreply.github.com>
2024-05-02 12:11:40 +0530

    new file University is created for aggregation Example

---
+++ University.java
@@ -0,0 +1,21 @@
+class University 
+{
+  String name ;
+  String univercity;
+  String noofColleges;
+ String type;
+ String establishedyear ;
+String course;
+
+University (String name, String university, String type, String noofColleges, String type, String establishedyear , String course){
+    
+
+   this.name=name;
+   this.univercity=university;
+   this.noofcolleges=noofColleges;
+   this.type=type;
+   this.establishedyear= establishedyear;
+   this.course= course;
+
+}
+}
